ddcf8b78609e2d2672ba977dd08659f5b2aa758e
geoffrey woodcock <theatomproject@gmail.com>
2018-04-10 17:54:47 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(string)
-  if string 
+def speak_to_grandma(phrase)
+  if phrase != phrase.upcase
    puts "HUH?! SPEAK UP, SONNY"
   end
 end
