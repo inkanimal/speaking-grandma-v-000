bb999cb08ea4c967acb5a10f6a6b7ac2e6d0bf50
geoffrey woodcock <theatomproject@gmail.com>
2018-04-10 17:45:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if "#{string}.upcase" == "I LOVE YOU GRANDMA!"
+  if "#{string}.upcase" 
     puts "I LOVE YOU TOO PUMPKIN!"
 
   end
